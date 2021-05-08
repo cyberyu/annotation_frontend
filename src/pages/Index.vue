efaf7a193630a94fb07afd382e0411e7be063ff1
Mingfeng Yang <mfyang@gmail.com>
2021-05-08 14:30:39 -0700

    initial api call

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,32 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+    <div class="row col-12">
+      <div class="row justify-center col-12">
+        <q-btn color="primary" label="Get Data" class="justify-center" @click="fetchDocs()"/>
+      </div>
+      <div v-for="(doc,i) in documents" :key="i" class="col-12 q-px-md q-py-md">
+        {{doc.text}}
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      documents: []
+    }
+  },
+  mounted () {
+  },
+  methods: {
+    fetchDocs () {
+      this.$axios.get('http://localhost:8000/documents/').then(response => {
+        this.documents = response.data.results
+      })
+    }
+  }
 }
 </script>
