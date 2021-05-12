eee675fe77fc695f57e89cd2b2cef44ea9479871
Mingfeng Yang <mfyang@gmail.com>
2021-05-12 10:27:13 -0700

    need to use ip, otherwise cors issue

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -6,5 +6,5 @@ Vue.prototype.$axios = axios
 if (process.env.PROD === true) {
   Vue.prototype.$hostname = window.location.origin + window.location.pathname
 } else {
-  Vue.prototype.$hostname = 'http://localhost:8000'
+  Vue.prototype.$hostname = 'http://127.0.0.1:8000'
 }
