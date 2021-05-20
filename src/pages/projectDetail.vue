1b5e142020aec80600ab462839f8a1d63081a26f
Mingfeng Yang <mfyang@gmail.com>
2021-05-20 09:01:27 -0700

    project and project detail

--- src/pages/projectDetail.vue
+++ src/pages/projectDetail.vue
@@ -1,14 +1,36 @@
 <template>
   <q-page class="flex flex-center">
-    <div class="row col-12">
-      <router-link to="/annotate?id=222">project detail</router-link>
+    <div class="row col-12 justify-center">
+      <h6>{{project.name}}</h6>
+      <div class="col-12"> {{ project.description }}</div>
+      <div class="row justify-center col-12 q-mt-md">
+        <q-btn color="primary" @click="$router.push({path: 'annotate', query: {id: project.id}})">
+          Annotate It
+        </q-btn>
+      </div>
     </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'projectDetail'
+  name: 'projectDetail',
+  data () {
+    return {
+      project: null
+    }
+  },
+  mounted () {
+    const id = this.$route.query.id
+    this.fetchProject(id)
+  },
+  methods: {
+    fetchProject (id) {
+      this.$axios.get(this.$hostname + `/projects/${id}/`).then(response => {
+        this.project = response.data
+      })
+    }
+  }
 }
 </script>
 
--- src/pages/projectList.vue
+++ src/pages/projectList.vue
@@ -1,7 +1,9 @@
 <template>
   <q-page class="flex flex-center">
     <div class="row col-12">
-      <router-link to="/project?id=222">project list</router-link>
+      <div v-for="(proj,i) in projects" :key="i" class="col-12">
+        <router-link :to="{path: 'project', query: {id: proj.id}}"> {{proj.name}} </router-link>
+      </div>
     </div>
   </q-page>
 
@@ -9,7 +11,22 @@
 
 <script>
 export default {
-  name: 'projectList'
+  name: 'projectList',
+  data () {
+    return {
+      projects: null
+    }
+  },
+  mounted () {
+    this.fetchProjects()
+  },
+  methods: {
+    fetchProjects () {
+      this.$axios.get(this.$hostname + '/projects/').then(response => {
+        this.projects = response.data.results
+      })
+    }
+  }
 }
 </script>
 
