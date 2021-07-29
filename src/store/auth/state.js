538ebfa710aeb4657925a177bf4c37b8e3d1bbc7
Mingfeng Yang <mfyang@gmail.com>
2021-07-29 12:40:06 -0700

    fix login status when refresh

--- src/store/auth/state.js
+++ src/store/auth/state.js
@@ -3,7 +3,7 @@ import { Cookies } from 'quasar'
 export default function () {
   return {
     // in backend SESSION_COOKIE_HTTPONLY = False
-    authenticated: Cookies.get('sessionid') || false,
+    authenticated: Cookies.get('vssid') || false,
     user: JSON.parse(localStorage.getItem('user') || '{}')
   }
 }
